d5cbaaf244830e05a19fd3ed9f7dd8c408e52134
piotrgrochowiecki <piotrgrochowiecki@gmail.com>
2022-04-10 11:50:03 +0200

    Spring. Dzień 2. Przekazywanie parametrów. Zadanie 1

--- src/main/java/pl/coderslab/RandomController.java
+++ src/main/java/pl/coderslab/RandomController.java
@@ -2,12 +2,22 @@ package pl.coderslab;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.Random;
 
 @Controller
 public class RandomController {
+
+    private final Random random = new Random();
+
+    @GetMapping(value = "/random//{max}", produces = "text/plain;charset=UTF-8")
+    @ResponseBody
+    public String showRandomWithMax(@PathVariable int max) {
+        int randomNumber = random.nextInt(max) + 1;
+        return "Użytkownik podał wartość " + max + ". Wylosowano liczbę " + randomNumber;
+    }
 
     @GetMapping(value = "/show-random", produces = "text/plain;charset=UTF-8")
     @ResponseBody
